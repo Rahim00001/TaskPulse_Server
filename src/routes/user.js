cafca2f9ffe604252afdb10a9d284d4b42fa79fb
Rahim00001 <rahimlpt2022@gmail.com>
2024-04-04 00:14:13 +0600

    jwt installed and login completed with jwt

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
-const User = require('../models/User')
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 router.post('/signup', async (req, res) => {
     const { username, email, password } = req.body;
@@ -17,7 +18,22 @@ router.post('/signup', async (req, res) => {
         password: hashpassword,
     })
     await newUser.save()
-    return res.json({ message: "record registerd" })
+    return res.json({ status: true, message: "record registerd" })
+})
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email })
+    if (!user) {
+        return res.json({ message: "user is not registerd" })
+    }
+    const validPassword = await bcrypt.compare(password, user.password)
+    if (!validPassword) {
+        return res.json({ message: "password is incorrect" })
+    }
+
+    const token = jwt.sign({ username: user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+    res.cookie('token', token, { httpOnly: true, maxAge: 3600000 })
+    return res.json({ status: true, message: "login successfully" })
 })
 
 module.exports = router
